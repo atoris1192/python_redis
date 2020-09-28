0c2b75644f28443e1c5a4dcdc8f514de10f35be2
atoris1192 <atoris1192@gmail.com>
2020-09-28 23:16:08 +0900

    app fix

--- app/app.py
+++ app/app.py
@@ -1,1 +1,10 @@
 print("Hello python!!!")
+
+import redis
+r = redis.Redis(host='redis', port=6379, db=0)
+
+r.set('hoge', 'moge')
+hoge = r.get('hoge')
+print(hoge.decode())
+
+result = r.delete('hoge')
